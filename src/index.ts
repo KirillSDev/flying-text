cf16ae1ca90ba9fe46fad72f824f4ff3e8497eca
Kirill Starodubsev <colereteps@gmail.com>
2024-02-07 00:20:58 +0400

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,72 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import './styles.css';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 
 //Canvas
 const canvas = document.getElementById('webgl') as HTMLCanvasElement;
 
 
+//Lights
+const light = new THREE.AmbientLight( 0x404040, 10);
+light.position.y = 5;
 
-//Geometry
-const box = new THREE.BoxGeometry(1, 1, 1);
+
+//Texture
+const textureLoader = new THREE.TextureLoader();
+const matcampTexture = textureLoader.load('./textures/matcaps/3.png');
+matcampTexture.colorSpace = THREE.SRGBColorSpace;
 
 //Meterial
-const mainMaterial = new THREE.MeshBasicMaterial({
-    wireframe: true,
+const mainMaterial = new THREE.MeshMatcapMaterial({
+    matcap: matcampTexture
 })
 
 
-//Mesh
-const boxMesh = new THREE.Mesh(box, mainMaterial);
+// //Mesh
+// const boxMesh = new THREE.Mesh(box, mainMaterial);
 
+const fontLoader = new FontLoader();
 // Scene
 const scene = new THREE.Scene();
 
-scene.add(boxMesh);
+scene.add(light);
+fontLoader.load('./fonts/Heyhoo_Regular.json', 
+    (font)=>{
+        const textGeometry = new TextGeometry("Crazyyy :)", {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 16,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 6
+            
+        })
+        textGeometry.center();
+        const textMesh = new THREE.Mesh(textGeometry, mainMaterial);
+       
+        scene.add(textMesh);
+    })
 
+
+
+for(let i = 0; i < 100; i++){
+    const random = () => (Math.random() - 0.5) * 10;
+    const torus = new THREE.TorusGeometry(1, (Math.random() - 0.4), 16);  
+    const torusMesh = new THREE.Mesh(torus, mainMaterial);  
+    const scale = (Math.random() - 0.5) * 2
+    torus.scale(scale, scale, scale);
+    torusMesh.position.x = random();
+    torusMesh.position.y = random();
+    torusMesh.position.z = random();
+    torusMesh.rotation.x = random();
+    torusMesh.rotation.y = random();
+    scene.add(torusMesh);
+}
 
 // Object
 const mainData = {
